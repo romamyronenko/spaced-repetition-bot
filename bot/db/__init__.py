f2bace102b6494fa31f9e574f69035968fb07293
romamyronenko <eoma575@gmail.com>
2024-01-07 01:41:31 +0200

    add docker

--- bot/db/__init__.py
+++ bot/db/__init__.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from .dbmanager import DBManager
 from .schema import Base
 
-DB_URI = "mysql+pymysql://roma:1@localhost/spaced_repetition"
+DB_URI = "mysql+pymysql://root:1@mysql/spaced_repetition"
 engine = create_engine(DB_URI, echo=False, future=True)
 Base.metadata.create_all(engine)
 db_manager = DBManager(engine)
